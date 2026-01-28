246ca2c7a807e71d05e1e69386ca93ed28766c2e
ekc395 <ekc395@gmail.com>
2026-01-27 18:22:47 -0800

    Initial commit

---
+++ lifecounter/lifecounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterApp.swift
+//  lifecounter
+//
+//  Created by Ethan Chen on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct lifecounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
