ad38b059b4f29586a6cd1a697022460007ed02ce
ekc395 <ekc395@gmail.com>
2026-02-01 22:55:08 -0800

    Center content on ipad

--- lifecounter/ContentView.swift
+++ lifecounter/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollView {
                 VStack {
                     Button("Add Player") {
